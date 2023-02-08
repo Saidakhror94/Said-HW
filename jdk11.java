bc926d58fb93b944d3fe78a533e3e5e3bd7c0859
Saidakhror94 <124196846+Saidakhror94@users.noreply.github.com>
2023-02-08 21:17:59 +0100

    Add files via upload
    Хотел на прямую через intelllije IDEA но к сожелению не разобрался как их синхронизировать. Не удивляйтесь циклам while and int value
    Я самоучка и пытаюсь поскорее научиться JAVA. Жду коментарий

---
+++ jdk11.java
@@ -0,0 +1,9 @@
+public class jdk11 {
+    public static void main(String [] args){
+     int value = 15;
+     while (value<100){
+         value = value+15;
+         System.out.println("hallo");
+         }
+     }
+}
---
+++ jdk17.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+
+public class jdk17 {
+    public static void main(String[] args){
+        int x = 28;
+        while (x>5) {
+            x = x-1;
+            String s = "Andrey";
+            String space = "";
+            String H = "Hallo";
+            System.out.println(H + space + s);
+            System.out.println("I am " + " Said" + " my nummer " + x);
+        }
+    }
+}
