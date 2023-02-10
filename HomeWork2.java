812ac4c0dee037c3e67510fcf013002b443bcfe2
Saidakhror94 <124196846+Saidakhror94@users.noreply.github.com>
2023-02-10 21:34:27 +0100

    Add files via upload
    выполнил задание на 3х значное число. Хотел уточнить как можно было написать лучше или короче

---
+++ HomeWork2.java
@@ -0,0 +1,14 @@
+package Lesson2;
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+
+        char n =365;
+    int e = n %10;
+    int d = n/10%10;
+    int s = n / 100 ;
+        System.out.println(e);
+        System.out.println(d);
+        System.out.println(s);
+    }
+}
